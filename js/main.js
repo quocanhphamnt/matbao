c141a42e2601454d0cb9c59e4e54fd419d2ac916
quocanhnt2001 <124774841+quocanhphamnt@users.noreply.github.com>
2023-05-08 10:39:31 +0700

    source

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+
+    var headBannerAds = new Swiper(".headBannerAds", {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+    });
+
+    $('#navbarCollapse .nav-link').on('click', event => {
+        $(event.currentTarget).next().toggle();
+        $(event.currentTarget).parents().siblings().find('.subMenu').hide();
+    });
+
+    $('.main-nav .nav-link').on('click', event => {
+        $(event.currentTarget).parents().siblings().find('.subMenu').hide();
+        $(event.currentTarget).parents().siblings().find('.nav-link').removeClass('active');
+        $(event.currentTarget).next().toggle();
+        $(event.currentTarget).toggleClass('active');
+    });
+
+    $('.main-nav').on('mouseleave', event => {
+        $(event.currentTarget).find('.subMenu').hide();
+        $(event.currentTarget).find('.nav-link').removeClass('active');
+    });
+
+    $('.lable').on('click', event => {
+        $(event.currentTarget).parents().siblings().find('.lable').next().hide();
+        $(event.currentTarget).next().toggle();
+    });
+
+    $( window ).on( "resize", function() {
+        if ($(window).width() > 768) {
+            $('.lable').next().show();
+        } else {
+            $('.lable').next().hide();
+        }
+    });
+});
